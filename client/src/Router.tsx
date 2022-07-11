05c98dfc24ad4a17835adf116731c7ef0a7736e6
[SW2/서울/2반]김한얼 <cjswogksdjf@gmail.com>
2022-07-11 02:37:29 +0000

    Update Router.tsx

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './pages/Login';
-import Logout from './pages/Logout';
-import FindPassword from './pages/FindPassword';
-import Signin from './pages/Signin';
-import About from './components/About';
-import NotFound from './components/NotFound';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Account from "./pages/Account";
+import Dashboard from "./pages/Dashboard";
+import About from "./components/About";
+import NotFound from "./components/NotFound";
 
 export function AppRouter() {
   return (
     <Router>
       <Routes>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/logout' element={<Logout />}></Route>
-        <Route path='/findpassword' element={<FindPassword />}></Route>
-        <Route path='/signin' element={<Signin />}></Route>
-        <Route path='/about' element={<About />} />
-        <Route path='*' element={<NotFound />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
