e98e5a7f2d4c7bae821f46c0c7c924577d25870b
노호연 <croookid@gmail.com>
2022-07-11 17:44:14 +0900

    [fix] 로그인시 passwordReset 프론트에 전달 로직 추가

--- backend/routers/user-router.js
+++ backend/routers/user-router.js
@@ -83,7 +83,11 @@ userRouter.post(
       }
       // jwt 토큰을 쿠키에 저장
       setUserToken(res, req.user);
-      res.status(200).json({ message: "로그인 성공", status: 200 });
+      res.status(200).json({
+        message: "로그인 성공",
+        status: 200,
+        passwordReset: req.user.passwordReset,
+      });
     } catch (error) {
       next(error);
     }
@@ -190,20 +194,6 @@ userRouter.get("/logout", async function (req, res, next) {
     next(error);
   }
 });
-
-// admin api로 이동 예정
-// 전체 유저 목록을 가져옴 (배열 형태임)
-// 미들웨어로 loginRequired 를 썼음 (이로써, jwt 토큰이 없으면 사용 불가한 라우팅이 됨)
-// userRouter.get('/userlist', loginRequired, async function (req, res, next) {
-//   try {
-//     // 전체 사용자 목록을 얻음
-//     const users = await userService.getUsers();
-//     // 사용자 목록(배열)을 JSON 형태로 프론트에 보냄
-//     res.status(200).json(users);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
 
 // get '/api/users/:userId'
 // 유저 정보 전달 api
