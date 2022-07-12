48d37a86441aa3812ddd078dbd4f838766ab350c
[SW2/서울/1반]심주용 <thelibra001@naver.com>
2022-07-12 20:04:14 +0900

    merge: confclit resolve

--- backend/app.js
+++ backend/app.js
@@ -12,22 +12,15 @@ dotenv.config();
 const PORT = process.env.SERVER_PORT || 8000;
 const app = express();
 
-app.listen(PORT, function () {
-  console.log(`listening on http://localhost:${PORT}`);
+const DB_URL = process.env.MONGODB_URL;
+mongoose.connect(DB_URL);
+mongoose.connection.on("connected", () => {
+    console.log("MongoDB connected");
 });
 
-const DB_URL =
-  process.env.MONGODB_URL ||
-  "MongoDB 서버 주소가 설정되지 않았습니다.\n./db/index.ts 파일을 확인해 주세요. \n.env 파일도 필요합니다.\n";
-
-mongoose.connect(DB_URL);
-const db = mongoose.connection;
-db.on("connected", () =>
-  console.log("정상적으로 MongoDB 서버에 연결되었습니다. " + DB_URL)
-);
-db.on("error", (error) =>
-  console.error("\nMongoDB 연결에 실패하였습니다...\n" + DB_URL + "\n" + error)
-);
+mongoose.connection.on("error", () => {
+    console.log("MongoDB error");
+});
 
 // CORS 에러 방지
 app.use(cors());
@@ -40,11 +33,14 @@ passportStrategies();
 
 // body parser 부분
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, function () {
+    console.log(`listening on http://localhost:${PORT}`);
+});
 
 // 테스팅용 라우터, 제거예정
 app.get("/", function (req, res) {
-  res.send("<h1>welcome page</h1>");
+    res.send("<h1>welcome page</h1>");
 });
 
 // passport 사용
