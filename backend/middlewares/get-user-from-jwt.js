b8c6b44ac5b855a995443da288c5e78f8f34d1a6
노호연 <croookid@gmail.com>
2022-07-12 17:36:22 +0900

    [fix] 유저 컨트롤러 분리, try catch를 asyncHandler로 변경

--- backend/middlewares/get-user-from-jwt.js
+++ backend/middlewares/get-user-from-jwt.js
@@ -1,8 +1,9 @@
 import passport from "passport";
-
+import { JWT_COOKIE_KEY } from "../utils";
 // jwt토큰이 있다면 이를 복호화하여 jwt strategie를 적용하는 미들웨어
+// req.user가 필요한 경우에 사용.
 const getUserFromJWT = (req, res, next) => {
-  if (!req.cookies.jwttoken) {
+  if (!req.cookies[JWT_COOKIE_KEY]) {
     next();
     return;
   }
--- backend/passport/strategies/kakao.js
+++ backend/passport/strategies/kakao.js
@@ -7,7 +7,7 @@ dotenv.config();
 const config = {
   clientID: process.env.KAKAO_CLIENT_ID,
   // callback url은 추후 수정하는걸로
-  callbackURL: `http://localhost:${process.env.SERVER_PORT}/api/auth/kauth`,
+  callbackURL: `/api/user/auth/kauth`,
 };
 
 // kakaoStrategy 부분
