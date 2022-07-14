01ac1c3583ade71554af169ca36dd8f584fbc9ef
aylee12 <aylee5117@gmail.com>
2022-07-14 16:00:58 +0900

    [update] 반응형 안내 구현(1120px 기준)

--- client/src/pages/Editor.tsx
+++ client/src/pages/Editor.tsx
@@ -1,43 +1,64 @@
-import styled from 'styled-components';
-import Sidebar from '../components/Sidebar';
-import Sidetab from '../components/Editor/Sidetab';
-import EditorSection from '../components/Editor/EditorSection';
-import { useSelector } from 'react-redux';
-import AlertModal from '../components/AlertModal';
-import ConfirmModal from '../components/ConfirmModal';
-import '../components/Editor/Font.css'
+import styled from "styled-components";
+import Sidebar from "../components/Sidebar";
+import Sidetab from "../components/Editor/Sidetab";
+import EditorSection from "../components/Editor/EditorSection";
+import { useSelector } from "react-redux";
+import AlertModal from "../components/AlertModal";
+import ConfirmModal from "../components/ConfirmModal";
+import "../components/Editor/Font.css";
 
-const Container = styled.div`
+const DesktopContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #f7f7f9;
+  display: flex;
+
+  @media screen and (max-width: 1120px) {
+    display: none;
+  }
+`;
+
+const MobileContainer = styled.div`
+  display: none;
+
+  @media screen and (max-width: 1120px) {
     width: 100vw;
     height: 100vh;
-    background-color: #f7f7f9;
     display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-family: "Elice Digital Baeum", sans-serif;
+    font-size: 30px;
+  }
 `;
 
 export default function Editor() {
-    const AlertModalState = useSelector(
-        (state: any) => state.alertReducer.state
-    );
+  const AlertModalState = useSelector((state: any) => state.alertReducer.state);
 
-    const alertData = useSelector(
-        (state: any) => state.alertReducer.alertData
-    );
+  const alertData = useSelector((state: any) => state.alertReducer.alertData);
 
-    const ConfirmModalState = useSelector(
-        (state: any) => state.modalReducer.isConfirmModal
-    );
+  const ConfirmModalState = useSelector(
+    (state: any) => state.modalReducer.isConfirmModal
+  );
 
-    const confirmData = useSelector(
-        (state: any) => state.modalReducer.confirmData
-    );
+  const confirmData = useSelector(
+    (state: any) => state.modalReducer.confirmData
+  );
 
-    return (
-        <Container>
-            <Sidebar />
-            <Sidetab />
-            <EditorSection />
-            {AlertModalState && <AlertModal alertData={alertData} />}
-            {ConfirmModalState && <ConfirmModal confirmData={confirmData} />}
-        </Container>
-    );
+  return (
+    <>
+      <DesktopContainer>
+        <Sidebar />
+        <Sidetab />
+        <EditorSection />
+        {AlertModalState && <AlertModal alertData={alertData} />}
+        {ConfirmModalState && <ConfirmModal confirmData={confirmData} />}
+      </DesktopContainer>
+      <MobileContainer>
+        <div>현재 <b>모바일 버전</b>은 지원하지 않고 있습니다.</div>
+        <div>빠른 시일 내에 <b>업데이트</b> 하도록 하겠습니다.</div>
+      </MobileContainer>
+    </>
+  );
 }
