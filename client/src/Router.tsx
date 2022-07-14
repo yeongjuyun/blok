2b79f490948a0397f5a3c65d37f5a314605288cd
dohveloper <dohveloper@gmail.com>
2022-07-14 09:57:12 +0900

    [feat] 테스트용 에디터페이지 추가

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,25 +1,27 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./pages/Login";
-import Account from "./pages/Account";
-import Dashboard from "./pages/Dashboard";
-import Admin from "./pages/Admin";
-import About from "./components/About";
-import NotFound from "./components/NotFound";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Account from './pages/Account';
+import Dashboard from './pages/Dashboard';
+import Admin from './pages/Admin';
+import About from './components/About';
+import NotFound from './components/NotFound';
 import Editor from './pages/Editor';
+import EditorTest from './pages/EditorTest';
 
 export function AppRouter() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path='/editor' element={<Editor />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/account" element={<Account />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/admin" element={<Admin />} />
+                <Route path="*" element={<NotFound />} />
+                <Route path="/editor" element={<Editor />} />
+                <Route path="/editorTest" element={<EditorTest />} />
+            </Routes>
+        </Router>
+    );
 }
---
+++ client/src/pages/EditorTest.tsx
@@ -0,0 +1,11 @@
+import Nav from '../components/Blocks/Simple/Nav/SettingBlock';
+
+import React from 'react';
+
+export default function EditorTest() {
+    return (
+        <div>
+            <Nav></Nav>
+        </div>
+    );
+}
