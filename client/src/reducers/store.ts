c3abd345aca72009db856d56e3c5c45e357f95cb
yeongjuyun <myyun1029@gmail.com>
2022-07-16 21:15:28 +0900

    [feat] 로그인 유저정보 리덕스로 관리

--- client/src/reducers/store.ts
+++ client/src/reducers/store.ts
@@ -1,11 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { userAuth } from './UserAuthReducer';
-import { sidetabReducer } from './SideTabReducer';
-import { alertReducer } from './AlertReducer';
-import { modalReducer } from './ModalReducer';
+import { configureStore } from "@reduxjs/toolkit";
+import { userAuth } from "./UserAuthReducer";
+import { sidetabReducer } from "./SideTabReducer";
+import { alertReducer } from "./AlertReducer";
+import { modalReducer } from "./ModalReducer";
+import { loginCheckReducer } from "./LoginCheckReducer";
 
 export const store = configureStore({
-  reducer: { userAuth, modalReducer, sidetabReducer, alertReducer },
+  reducer: {
+    userAuth,
+    loginCheckReducer,
+    modalReducer,
+    sidetabReducer,
+    alertReducer,
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
