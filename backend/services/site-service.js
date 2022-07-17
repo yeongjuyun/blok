a0e28236d7046d545dfdfd6dd1238e615a012f95
[SW2/서울/1반]심주용 <thelibra001@naver.com>
2022-07-17 16:05:07 +0900

    fix: site router, site model, service, controller by using siteId

--- backend/services/site-service.js
+++ backend/services/site-service.js
@@ -26,8 +26,8 @@ class SiteService {
   }
 
   // 특정 사이트 정보 조회
-  async getSiteInfo(siteIdentifier) {
-    const site = await this.siteModel.findBySiteName(siteIdentifier);
+  async getSiteInfo(siteId) {
+    const site = await this.siteModel.findBySiteId(siteId);
     if (!site) {
       throw new BadRequestError("존재하지 않는 사이트입니다.");
     }
@@ -45,27 +45,27 @@ class SiteService {
   }
 
   // 사이트 정보 수정
-  async updateSite(siteIdentifier, toUpdate) {
-    let site = await this.siteModel.findBySiteId(siteIdentifier);
+  async updateSite(siteId, toUpdate) {
+    let site = await this.siteModel.findBySiteId(siteId);
     if (!site) {
       throw new BadRequestError(
         "등록된 사이트가 없습니다. 다시 한 번 확인해 주세요."
       );
     }
-    let name = await this.siteModel.findBySiteName(toUpdate.name);
-    if (name && name.no !== siteIdentifier) {
-      throw new BadRequestError(
-        "이 사이트의 이름은 현재 사용중입니다. 다른 사이트 이름을 입력해 주세요"
-      );
-    }
-    let domain = await this.siteModel.findBySiteDomain(toUpdate.domain);
-    if (domain && domain.no !== siteIdentifier) {
-      throw new BadRequestError(
-        "이 사이트의 도메인은 현재 사용중입니다. 다른 도메인을 입력해 주세요"
-      );
-    }
+    // let name = await this.siteModel.findBySiteName(toUpdate.name);
+    // if (name && name.no !== siteId) {
+    //   throw new BadRequestError(
+    //     "이 사이트의 이름은 현재 사용중입니다. 다른 사이트 이름을 입력해 주세요"
+    //   );
+    // }
+    // let domain = await this.siteModel.findBySiteDomain(toUpdate.domain);
+    // if (domain && domain.no !== siteId) {
+    //   throw new BadRequestError(
+    //     "이 사이트의 도메인은 현재 사용중입니다. 다른 도메인을 입력해 주세요"
+    //   );
+    // }
     site = await this.siteModel.update({
-      id: siteIdentifier,
+      id: siteId,
       update: toUpdate,
     });
     return site;
