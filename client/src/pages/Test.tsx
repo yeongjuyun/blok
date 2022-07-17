8057f3de21bf7907aab6e22a85dc570d587c2197
dohveloper <dohveloper@gmail.com>
2022-07-17 09:55:58 +0900

    [fix] 테스트 페이지 추가

---
+++ client/src/pages/Test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Modal from '../components/Editor/Sidetab/AddModal/AddModal';
+
+export default function Testbed() {
+  return (
+    <div>
+      <Modal alertData='hi'></Modal>
+    </div>
+  );
+}
