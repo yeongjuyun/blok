c70fa258088093526dd0980c1c8b904e1ca49fa0
[SW2/서울/2반]김한얼 <gksdjf80@naver.com>
2022-07-18 15:13:13 +0900

    [fix] testpage

--- client/src/pages/Test.tsx
+++ client/src/pages/Test.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
-
+import React, { useState, useRef } from 'react';
 import { HeroSite, NavBar } from '../components/Sitebox';
+import { CustomSelect, TextInput } from '../components/Input';
 
 const Container = styled.div`
   padding-top: 50px;
@@ -15,7 +16,7 @@ const Container = styled.div`
   width: 100%;
   height: 100vh;
 `;
-const font = 'Roboto';
+const data = 'Roboto';
 const colorSet = {
   primary: '#482924',
   secondary: '#123456',
@@ -23,9 +24,32 @@ const colorSet = {
   surface: '#123456',
 };
 
+const options = [
+  { value: '스타일1', label: '스타일1' },
+  { value: '스타일2', label: '스타일2' },
+  { value: '스타일3', label: '스타일3' },
+];
 function Test() {
+  const [select, setSelect] = useState();
+  const Input = useRef<HTMLInputElement>(null);
   return (
     <>
+      {/* <CustomSelect
+        key={'123213'}
+        options={options}
+        onChange={(e: any) => {
+          console.log(e);
+          setSelect(e);
+        }}
+        value={select}
+        defaultValue={{ value: '스타일1', label: '스타일1' }}
+      ></CustomSelect> */}
+      <TextInput
+        ref={Input}
+        onChange={(e: any) => {
+          console.log(Input.current!.value);
+        }}
+      ></TextInput>
       <HeroSite></HeroSite>
     </>
   );
