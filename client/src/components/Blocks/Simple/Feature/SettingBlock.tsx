1990cb0d7a1562a62c82923d770b924e936d54e9
dohveloper <dohveloper@gmail.com>
2022-07-18 17:54:03 +0900

    [feat] 블록값 수정하기 기능을 위해useState & dispatch 추가

--- client/src/components/Blocks/Simple/Feature/SettingBlock.tsx
+++ client/src/components/Blocks/Simple/Feature/SettingBlock.tsx
@@ -18,11 +18,21 @@ function Feature({ block, onRemove }: Feature) {
   const blockType = block.template.blockType;
   let styleOptions = getStyleOptions(blockType);
 
-  const [input, setInput] = useState('');
-  const [selectinput, setSelectInput] = useState({
+  //Input
+  const [navTitle, setNavTitle] = useState(data.navTitle);
+  const [style, setStyle] = useState({
     label: data.style.value,
     value: data.style.value,
   });
+  const [caption, setCaption] = useState(data.caption?.value);
+  const [header, setHeader] = useState(data.header?.value);
+  const [headerHighlight, setHeaderHighlight] = useState(
+    data.headerHighlight?.value
+  );
+  const [body, setBody] = useState(data.body?.value);
+  const [buttonTitle, setButtonTitle] = useState(data.button?.title);
+  const [buttonUrl, setButtonUrl] = useState(data.button?.url);
+  const [image, setImageUrl] = useState(data.button?.url);
 
   return (
     <>
@@ -31,17 +41,17 @@ function Feature({ block, onRemove }: Feature) {
           title="메뉴명"
           required={true}
           onChange={(e: any) => {
-            console.log(e.target.value);
-            // dispatch(
-            //   updateBlockData({
-            //     blockId: block.id,
-            //     field: 'navTitle',
-            //     value: e.target.value,
-            //   })
-            // );
+            setNavTitle(e.target.value);
+            dispatch(
+              updateBlockData({
+                blockId: block.id,
+                field: 'navTitle',
+                value: e.target.value,
+              })
+            );
           }}
           guideline="네비게이션 바에 입력될 메뉴명을 입력하세요."
-          value={data.navTitle}
+          value={navTitle}
         ></TextInput>
         <CustomSelect
           title="스타일"
@@ -50,13 +60,16 @@ function Feature({ block, onRemove }: Feature) {
           placeholder="원하는 선택지를 선택해주세요"
           options={styleOptions}
           onChange={(e: any) => {
-            setSelectInput(e);
+            setStyle(e);
+            dispatch(
+              updateBlockData({ blockId: block.id, field: 'style', value: e })
+            );
           }}
-          value={selectinput}
+          value={style}
         />
         <ImgInput
           title="이미지"
-          guideline="헤드라인 내용 중에서 강조할 텍스트를 입력하세요"
+          guideline="사이트에 표시할 이미지를 업로드하세요"
           src={data.image?.src}
           alt={data.image?.alt}
         />
@@ -64,37 +77,97 @@ function Feature({ block, onRemove }: Feature) {
           title="캡션"
           required={false}
           guideline="캡션에 표시될 내용을 입력하세요."
-          value={data.caption?.value}
+          value={caption}
+          onChange={(e: any) => {
+            setCaption(e.target.value);
+            dispatch(
+              updateBlockData({
+                blockId: block.id,
+                field: 'caption',
+                value: { value: e.target.value },
+              })
+            );
+          }}
         ></TextInput>
         <TextInput
           title="헤드라인"
           required={false}
           guideline="헤드라인에 표시될 내용을 입력하세요."
-          value={data.header?.value}
+          value={header}
+          onChange={(e: any) => {
+            setHeader(e.target.value);
+            dispatch(
+              updateBlockData({
+                blockId: block.id,
+                field: 'header',
+                value: { value: e.target.value },
+              })
+            );
+          }}
         ></TextInput>
         <TextInput
           title="헤드라인 강조 테스트"
           required={false}
           guideline="헤드라인 내용 중에서 강조할 텍스트를 입력하세요"
-          value={data.headerHighlight?.value}
+          value={headerHighlight}
+          onChange={(e: any) => {
+            setHeaderHighlight(e.target.value);
+            dispatch(
+              updateBlockData({
+                blockId: block.id,
+                field: 'headerHighlight',
+                value: { value: e.target.value },
+              })
+            );
+          }}
         ></TextInput>
         <TextInput
           title="설명"
           required={false}
           guideline="설명에 표시될 내용을 입력하세요"
-          value={data.body?.value}
+          value={body}
+          onChange={(e: any) => {
+            setBody(e.target.value);
+            dispatch(
+              updateBlockData({
+                blockId: block.id,
+                field: 'body',
+                value: { value: e.target.value },
+              })
+            );
+          }}
         ></TextInput>
         <TextInput
           title="버튼 텍스트"
           required={false}
           guideline="비워둘 경우 버튼이 나타나지 않습니다."
-          value={data.button?.title}
+          value={buttonTitle}
+          onChange={(e: any) => {
+            setButtonTitle(e.target.value);
+            dispatch(
+              updateBlockData({
+                blockId: block.id,
+                field: 'button',
+                value: { title: e.target.value, url: buttonUrl },
+              })
+            );
+          }}
         ></TextInput>
         <TextInput
           title="버튼 URL"
           required={false}
           guideline="버튼 클릭시 이동될 url을 입력하세요"
-          value={data.button?.url}
+          value={buttonUrl}
+          onChange={(e: any) => {
+            setButtonUrl(e.target.value);
+            dispatch(
+              updateBlockData({
+                blockId: block.id,
+                field: 'button',
+                value: { title: buttonTitle, url: e.target.value },
+              })
+            );
+          }}
         ></TextInput>
       </Card>
     </>
--- client/src/reducers/SiteReducer.ts
+++ client/src/reducers/SiteReducer.ts
@@ -242,7 +242,8 @@ export const siteSlice = createSlice({
     ) => {
       const { blockId, field, value } = action.payload;
       let index = state.blocks.findIndex((block) => block.id === blockId);
-      console.log(state.blocks[index].data);
+      //state.blocks[index].data[field] = value;
+      console.log(blockId, field, value);
     },
   },
 });
