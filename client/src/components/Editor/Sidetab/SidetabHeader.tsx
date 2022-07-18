bdbfe65f9a97fd4da748613fa4e8ef4f7123035d
aylee12 <aylee5117@gmail.com>
2022-07-18 18:45:00 +0900

    [fix] SideTabHeader 오류 수정

--- client/src/components/Editor/Sidetab/SidetabHeader.tsx
+++ client/src/components/Editor/Sidetab/SidetabHeader.tsx
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../../reducers/hooks';
 
 const unclicked = '#999999';
 const clicked = '#000000';
@@ -18,39 +17,20 @@ const Container = styled.div`
 `;
 
 export default function SidetabHeader() {
-  const dispatch = useDispatch();
-  const now = useSelector((state: any) => state.sidetabReducer);
+  const dispatch = useAppDispatch();
+  const now = useAppSelector((state: any) => state.sidetabReducer);
   const toolList = ['Block', 'Appearance', 'Setting'];
   const itemWidth = 100 / toolList.length;
-
-  if (!now) {
-    dispatch({ type: toolList[0] });
-  }
 
   const Item = styled.div`
     width: ${itemWidth}%;
     text-align: center;
     font-weight: 650;
-    color: ${unclicked};
 
     :hover {
       cursor: pointer;
     }
   `;
-
-  useEffect(() => {
-    for (let i = 0; i < toolList.length; i++) {
-      const target = document.getElementById(toolList[i]);
-
-      if (target) {
-        if (toolList[i] === now) {
-          target.style.color = clicked;
-        } else {
-          target.style.color = unclicked;
-        }
-      }
-    }
-  }, [now]);
 
   function Items() {
     let items = [];
@@ -59,6 +39,7 @@ export default function SidetabHeader() {
         <Item
           key={toolList[i]}
           id={toolList[i]}
+          style={{ color: toolList[i] === now ? clicked : unclicked }}
           onClick={() => dispatch({ type: toolList[i] })}
         >
           {toolList[i]}
