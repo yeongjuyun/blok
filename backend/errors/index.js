ec5e19bcf1296fb852c5e25b0c78bc714a85fdfa
노호연 <croookid@gmail.com>
2022-07-19 15:31:19 +0900

    [fix] login-required 로직 변경

--- backend/errors/index.js
+++ backend/errors/index.js
@@ -2,4 +2,3 @@ export * from "./custom-error";
 export * from "./bad-request-error";
 export * from "./forbidden-error";
 export * from "./not-found-error";
-export * from "./internal-server-error";
--- backend/middlewares/login-required.js
+++ backend/middlewares/login-required.js
@@ -1,10 +1,10 @@
 import passport from "passport";
-import { InternalServerError, ForbiddenError } from "../errors";
+import { ForbiddenError } from "../errors";
 
 function loginRequired(req, res, next) {
   return passport.authenticate("jwt", { session: false }, (authErr, user) => {
     if (authErr) {
-      throw new InternalServerError("Internal Server Error: " + authErr);
+      next(authErr);
     }
     if (!user) {
       throw new ForbiddenError("로그인한 유저만 사용할 수 있는 서비스입니다.");
