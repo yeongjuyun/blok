f32f67555f9665945118c434a8ecbb894e1f0872
aylee12 <aylee5117@gmail.com>
2022-07-20 16:19:43 +0900

    [update] 심플테마 feature, footer siteBlock 수정

--- client/src/components/Blocks/Simple/Footer/SiteBlock.tsx
+++ client/src/components/Blocks/Simple/Footer/SiteBlock.tsx
@@ -1,28 +1,48 @@
 import styled from 'styled-components';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../../reducers';
+import { SiteBlockProps, ColorSet } from '../../blockValidator';
+import { selectBlockById } from '../../../../reducers/SiteReducer';
 
-export default function SiteBlock(props: any) {
-  const Container = styled.div`
-    height: 50px;
-    background-color: ${props.colorSet.background};
-    font-family: ${props.font};
-    color: ${props.colorSet.surface};
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 40px;
-    font-size: 0.8rem;
+const Container = styled.div<{ colorSet: ColorSet; font: string }>`
+  background-color: ${(props) => props.colorSet.background};
+  font-family: ${(props) => props.font};
+  color: ${(props) => props.colorSet.surface};
 
-    @media screen and (max-width: 1120px) {
-      font-size: 1.5vw;
-      padding: 0 20px;
-    }
-  `;
+  height: 50px;
+  background-color: ${(props) => props.colorSet.background};
+  font-family: ${(props) => props.font};
+  color: ${(props) => props.colorSet.surface};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 40px;
+  font-size: 0.8rem;
+
+  @media screen and (max-width: 1120px) {
+    font-size: 1.5vw;
+    padding: 0 20px;
+  }
+`;
+
+export default function SiteBlock(props: SiteBlockProps) {
+  const { blockId } = props;
+
+  const { data } = useSelector((state: RootState) =>
+    selectBlockById(state, blockId)
+  );
+  const colorSet = useSelector((state: RootState) => state.site.colorSet);
+  const font = useSelector((state: RootState) => state.site.font);
 
   return (
     <>
-      <Container>
-        <p>{props.data.leftText.value}</p>
-        <p>{props.data.rightText.value}</p>
+      <Container
+        colorSet={colorSet}
+        font={font}
+        id={data.navTitle !== null ? data.navTitle : ''}
+      >
+        <p>{data.leftText?.value}</p>
+        <p>{data.rightText?.value}</p>
       </Container>
     </>
   );
